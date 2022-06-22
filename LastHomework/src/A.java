ff69ed0e267b516136ee6e2ad508516d933dde89
Dong <djlee1201@naver.com>
2022-06-22 22:33:47 +0900

    first commit in devC branch

--- LastHomework/src/A.java
+++ LastHomework/src/A.java
@@ -3,5 +3,6 @@ public class A {
 	public static void main(String[]  args) {
 		System.out.println("This is A");
 		System.out.println("A is Changed");
+		System.out.println("A.java Change in devC");
 	}
 }
--- LastHomework/src/C.java
+++ LastHomework/src/C.java
@@ -2,5 +2,6 @@
 public class C {
 	public static void main(String[]  args) {
 	System.out.println("This is C");
+	System.out.println("C.java Change in devC");
 	}
 }
